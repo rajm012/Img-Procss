a80bfa9305b342aca497e423fbce37c2c4d4194f
Ajjubook <rajmahimaurya@gmail.com>
2024-12-29 19:17:15 +0530

    Updated Charts'

--- common/utils.js
+++ common/utils.js
@@ -2,6 +2,17 @@ const utils = {};
 
 utils.flaggedUsers = [1663053145814,1663855324959,1663855328187];
 
+utils.styles = {
+    car: {color: 'gray',text:""},
+    fish: {color: 'red',text:""},
+    house: {color: 'yellow',text:""},
+    tree: {color: 'green',text:""},
+    bicycle: {color: 'cyan',text:""},
+    guitar: {color: 'blue',text:""},
+    pencil: {color: 'magenta',text:""},
+    clock: {color: 'lightgray',text:""}
+}
+
 utils.formatPercent =(n) =>{
     return (n*100).toFixed(2) + '%';
 }
